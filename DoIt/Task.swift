f1c886acc6f16e11da6fc5f9fee58c2a7666ce2a
Jason Cox <jasoncox@mac.com>
2018-02-10 21:55:53 -0500

    Added Task Class and 2nd window with segues.

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Jason Cox on 2/10/18.
+//  Copyright © 2018 Jason Cox. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
